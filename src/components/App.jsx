9233bad94c19448369406b90a2f8b37377d51ec9
Mihir Panchal <mihirpanchal2603@gmail.com>
2023-03-05 13:34:47 +0530

    Add the ability to add and delete new notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,43 @@
-import React from "react";
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
-import notes from '../notes';
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            {/* Rendering all notes via map function */}
-            {notes.map( (note) =>  (<Note key={note.key} title={note.title} content={note.content} />) )}
-            <Footer />
-        </div>
-    );
+  const [notes, setNotes] = useState([]);
+  function handleSubmit(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function handleDelete(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((prevNote, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={handleSubmit} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={handleDelete}
+          />
+        );
+      })}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    setNote({ title: "", content: "" });
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleChange}
+          value={note.content}
+        />
+        <button
+          onClick={() => {
+            props.onAdd(note);
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
