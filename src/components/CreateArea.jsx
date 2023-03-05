17cd3a958d0dd5cce83748182de7de87aa1c6329
Mihir Panchal <mihirpanchal2603@gmail.com>
2023-03-05 14:44:24 +0530

    Styling using Material UI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -6,6 +9,13 @@ function CreateArea(props) {
     content: ""
   });
 
+//   dynamically trace the title and content input fields
+// and set note object from the above state to whatever user typed
+// and return the object using spread operator ... to first add
+// whatever user wrote like title or content then use [] operator
+// to use target events name as a key for adding inside the object
+// so if user latest edited content the name will be content: and value
+// will be whatever they typed and ...prevNote will be title: whatever they typed
   function handleChange(event) {
     const { name, value } = event.target;
     setNote((prevNote) => {
@@ -16,33 +26,57 @@ function CreateArea(props) {
     });
   }
 
+//   when submit button is clicked I will trigger this function
+// to prevent default refresh by React using event.preventDefault();
   function handleSubmit(event) {
-    setNote({ title: "", content: "" });
     event.preventDefault();
   }
+
+//   state for handling conditional rendering of zoom animation
+// as well as title input field rendering
+  const[isFocus, setFocus] = useState(false);
+  function showTitle() {
+    setFocus(true);
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={note.title}
-        />
+      <form onSubmit={handleSubmit} className="create-note">
+      {/* conditional rendering to render input only when user clicks textarea */}
+        {
+            isFocus 
+            && 
+            (<input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={note.title}
+            />)
+        }
         <textarea
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isFocus ? "3" : "1"}
           onChange={handleChange}
           value={note.content}
+          onClick={showTitle}
         />
-        <button
-          onClick={() => {
-            props.onAdd(note);
-          }}
-        >
-          Add
-        </button>
+        <Zoom in={isFocus}>
+            <Fab
+            // onclick we call psuedo function
+            // inside which we received a function as a prop from
+            // App component and we call that function from here and pass
+            // the currently added note to App component where we add it to array
+            // of notes objects and render it using .map() inside App component
+            // and resent the title and content input fields using setNote state handler
+            onClick={() => {
+                props.onAdd(note);
+                setNote({ title: "", content: "" });
+            }}
+            >
+            <AddIcon />
+            </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 function Header() {
     return (
         <header>
             <h1>
-                Keeper
+                <HighlightIcon /> Keeper
             </h1>
         </header>
     );
